edfc8b3dcc57a21d4d6a3e3490e49f0e84880b1c
Bhargav S <bhargavbharu447@gmail.com>
2025-03-09 15:51:39 +0530

    added a controller to get the channel details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -388,22 +388,81 @@ if(!coverImage.url) {
    )
 })
 
-const getUserChannelDetails = asyncHandler(async (req,res) => {
-      const {username} =  req.params
+const getUserChannelDetails = asyncHandler(async (req, res) => {
 
-      if(!username) {
-        throw new ApiError(401,"username is required")
+   const {username} = req.params
+
+   if(!username?.trim()){
+    throw new ApiError(400 , "username is required")
+   }
+
+  const channel =  await User.aaggreagate([
+    {
+      $match :{
+        username : username?.toLowerCase()
       }
-
-     await User.aggregate([
-        {
-          $match : {
-            username : username?.lowercase()
-          }
+    },
+    {
+      $lookup : {
+        from : "subscriptions",
+        localField : "_id",
+        foreignField : "channel",
+        as : "subscribers"
+      }
+    },
+    {
+      $lookup : {
+        from : "subscriptions",
+        localField : "_id",
+        foreignField : "subscriber",
+        as : "subscribedTo"
+      }
+    },
+    {
+      $addFields : {
+        subscriberCount : { 
+          $size : "$subscribers"
         },
-        {},
-      ])
+        subscribedToCount : { 
+          $size : "$subscribedTo"
+        },
+        isSubscribed : {
+          $cond : {
+            if : {$in : [req.user._id, "$subscribers.subscriber"]},
+            then : true,
+            else : false
+          }
+        }
+      }
+    },
+    {
+      $project : {
+        username : 1,
+        email : 1,
+        fullname  : 1,
+        avatar : 1,
+        coverImage : 1,
+        subscriberCount : 1,
+        subscribedToCount : 1,
+        isSubscribed : 1,
+      }
+    }
+   ])
 
+   if(!channel?.length) {
+    throw new ApiError(404 , "channel does not exist")
+   }
+
+   return res
+   .status(200)
+   .json(
+    new ApiResponse(
+      200,
+       channel[0] ,
+      "channel details fetched successfully"
+    )
+   )
+});
 
 
 export { 
