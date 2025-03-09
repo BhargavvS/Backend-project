b519b33bb7045c2b8424587bbc06326c7474f896
Bhargav S <bhargavbharu447@gmail.com>
2025-03-09 22:00:07 +0530

    added user routers

--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -1,5 +1,16 @@
 import {Router} from "express";
-import { registerUser , loginUser , loggoutUser  , RefreshAccessToken} from '../controllers/user.controller.js';
+import { registerUser , 
+    loginUser , 
+    loggoutUser  , 
+    RefreshAccessToken , 
+    updatePassword , 
+    getCurrentUser ,
+    updateUserAccountDetails, 
+    upadeUserAvatar,
+    updateUserCoverImage ,
+     getUserChannelDetails , 
+     getWatchHistory
+    } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';   
 import {validateJWT} from '../middlewares/auth.middleware.js';
 
@@ -25,6 +36,13 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(validateJWT , loggoutUser)
 
 router.route("/refresh-token").post(RefreshAccessToken)
-
+router.route("/change-password").post(validateJWT , updatePassword)
+router.route("/current-user").get(validateJWT , getCurrentUser)
+router.route("/update-user").post(validateJWT , updateUserAccountDetails)
+router.route("/avatar").post(validateJWT , upload.single("avatar") , upadeUserAvatar)
+router.route("/cover-image").post(validateJWT , upload.single("coverImage") , updateUserCoverImage)
+router.route("/c/:username").get(validateJWT , getUserChannelDetails)
+router.route("/channel-details").get(validateJWT , getUserChannelDetails)
+router.route("/history").get(validateJWT , getWatchHistory)
 
 export default router;
