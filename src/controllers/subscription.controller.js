a8a141216958979ee6e42d08e1daa24f385eb579
Bhargav S <bhargavbharu447@gmail.com>
2025-03-24 00:16:19 +0530

    add subscription toggle functionality; validate channel ID and handle subscription creation and deletion

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,6 +9,50 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // toggle subscription
+
+    if(!isValidObjectId(channelId)) {
+        throw new ApiError(400, "Channel ID is required")
+    }
+
+  const subscribed = await  Subscription.findOne({
+        channel : channelId,
+        subscriber : req.User._id
+    })
+
+    if(subscribed) {
+       try {
+        await Subscription.findByIdAndDelete(subscribed._id)
+
+        return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200 , {} , "Unsubscribed the channel sucessfully"
+            )
+        )
+       } catch (error) {
+              throw new ApiError(500, "Subscription not removed")
+       }
+    }
+
+    if(!subscribed) {
+         const newSubscription =   await Subscription.create(
+               { subscriber : req.User._id,
+                channel : channelId}
+            )
+        
+            if(!newSubscription) {
+                throw new ApiError(500 , "Unsucessful Subscription")
+            }
+
+            return res
+            .status(200)
+            .json(
+                new ApiResponse(
+                    200 , newSubscription , "Subscribed to the channel successfully"
+                )
+            )
+    }
 })
 
 // controller to return subscriber list of a channel
